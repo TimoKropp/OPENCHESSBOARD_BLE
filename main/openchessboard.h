aaa51bee6836bc3af71b17dff7934ccf9abdf351
TimoKropp <47632407+TimoKropp@users.noreply.github.com>
2022-10-05 09:06:15 +0200

    Delete openchessboard.h

--- main/openchessboard.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-extern void initHW(void);
-extern String getMoveInput(void);
-extern void clearDisplay(void);
-extern void displayMove(String mv);
-extern void displayConnectWait(void);
-extern void displayBtoPlay(void);
-extern void displayWtoPlay(void);
