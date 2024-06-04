de0705284f9b8e86d2a87e26e4e514cb683ffcc8
TimoKropp <47632407+TimoKropp@users.noreply.github.com>
2024-06-04 15:18:36 +0200

    Update openchessboard.h
    adde new animation

--- src/openchessboard.h
+++ src/openchessboard.h
@@ -5,5 +5,4 @@ extern String getMoveInput(void);
 extern void clearDisplay(void);
 extern void displayMove(String mv);
 extern void displayConnectWait(void);
-extern void displayBtoPlay(void);
-extern void displayWtoPlay(void);
+extern void displayNewGame(void);
